5e218566a76f71b8f7f4e2d855fb339ff10783fb
Edwin Dominguez <edwindominguez@hotmail.com>
2022-01-08 21:05:51 -0500

    refactor creation status for post methods

--- src/index.js
+++ src/index.js
@@ -12,14 +12,14 @@ app.use(express.json())
 app.post('/users', (req, res) => {
   const user = new User(req.body)
   user.save()
-    .then(() => res.send(user))
+    .then(() => res.status(201).send(user))
     .catch(e => res.status(400).send(e))
 })
 
 app.post('/tasks', (req, res) => {
   const task = new Task(req.body)
   task.save()
-    .then(() => res.send(task))
+    .then(() => res.status(201).send(task))
     .catch(e => res.status(400).send(e))
 })
 
