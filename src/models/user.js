d9172eb1fb29592505eda62b9e01ef003e49ef71
Edwin Dominguez <edwindominguez@hotmail.com>
2022-01-09 15:24:01 -0500

    add bcrypt and encryption for passwords, refactor update logic

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
@@ -22,7 +23,6 @@ const User = mongoose.model('User', {
     minlength: 7,
     trim: true,
     validate(value) {
-      console.log('Pass', value)
       if (value.toLowerCase().includes('password')) throw new Error('Password can not contain "password"')
     }
   },
@@ -34,5 +34,15 @@ const User = mongoose.model('User', {
     }
   }
 })
+
+userSchema.pre('save', async function (next) {
+  const user = this
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8)
+  }
+  next()
+})
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
--- src/routes/user.js
+++ src/routes/user.js
@@ -43,9 +43,10 @@ router.patch('/users/:id', async (req, res) => {
 
   const _id = req.params.id
   try {
-    const user = await User.findByIdAndUpdate(_id, req.body,
-      { new: true, runValidators: true })
+    const user = await User.findById(_id)
     if (!user) return res.status(404).send()
+    updates.forEach(update => user[update] = req.body[update])
+    await user.save()
     res.send(user)
   } catch (e) {
     res.status(400).send(e)
