9fb938bc11dbac281ca404abcc5d3de9bae9b24e
Edwin Dominguez <edwindominguez@hotmail.com>
2022-01-08 21:02:49 -0500

    refactor task model, add task post endpoint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,9 +13,14 @@ app.post('/users', (req, res) => {
   const user = new User(req.body)
   user.save()
     .then(() => res.send(user))
-    .catch(e => {
-      res.status(400).send(e)
-    })
+    .catch(e => res.status(400).send(e))
+})
+
+app.post('/tasks', (req, res) => {
+  const task = new Task(req.body)
+  task.save()
+    .then(() => res.send(task))
+    .catch(e => res.status(400).send(e))
 })
 
 app.listen(port, () => {
