3d7b8a9eca30a44218aaf1d1b1a0a6694e8da062
Edwin Dominguez <edwindominguez@hotmail.com>
2022-01-09 22:54:08 -0500

    refactor user router methods

--- src/routes/user.js
+++ src/routes/user.js
@@ -50,18 +50,7 @@ router.get('/users/me', auth, async (req, res) => {
   res.send(req.user)
 })
 
-router.get('/users/:id', async (req, res) => {
-  const _id = req.params.id
-  try {
-    const user = await User.findById(_id)
-    if (!user) return res.status(404).send()
-    res.send(user)
-  } catch (e) {
-    res.status(500).send(e)
-  }
-})
-
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
   const updates = Object.keys(req.body)
   const allowedUpdates = ['name', 'email', 'password', 'age']
   const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -69,11 +58,8 @@ router.patch('/users/:id', async (req, res) => {
   if (!isValidOperation) {
     return res.status(400).send({ error: 'Invalid updates!' })
   }
-
-  const _id = req.params.id
   try {
-    const user = await User.findById(_id)
-    if (!user) return res.status(404).send()
+    const user = req.user
     updates.forEach(update => user[update] = req.body[update])
     await user.save()
     res.send(user)
@@ -82,12 +68,10 @@ router.patch('/users/:id', async (req, res) => {
   }
 })
 
-router.delete('/users/:id', async (req, res) => {
-  const _id = req.params.id
+router.delete('/users/me', auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(_id)
-    if (!user) return res.status(404).send()
-    res.send(user)
+    await req.user.remove()
+    res.send(req.user)
   } catch (e) {
     res.status(500).send(e)
   }
