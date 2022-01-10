86cd35dea9724ab80932650bca514dd21b65ad9a
Edwin Dominguez <edwindominguez@hotmail.com>
2022-01-10 15:16:34 -0500

    add auth for tasks endpoints

--- src/routes/task.js
+++ src/routes/task.js
@@ -16,19 +16,22 @@ router.post('/tasks', auth, async (req, res) => {
   }
 })
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
   try {
-    const tasks = await Task.find()
+    await req.user.populate('tasks').execPopulate()
+    const tasks = req.user.tasks;
+    if (tasks.length === 0) return res.status(204).send()
     res.send(tasks)
   } catch (e) {
+    console.log(e)
     res.status(500).send(e)
   }
 })
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
   const _id = req.params.id
   try {
-    const task = await Task.findById(_id)
+    const task = await Task.findOne({ _id, owner: req.user._id })
     if (!task) return res.status(404).send()
     res.send(task)
   } catch (e) {
@@ -36,16 +39,15 @@ router.get('/tasks/:id', async (req, res) => {
   }
 })
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
   const updates = Object.keys(req.body)
   const allowedUpdates = ['description', 'completed']
   const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
 
   if (!isValidOperation) return res.status(400).send({ error: 'Invalid updates!' })
 
-  const _id = req.params.id
   try {
-    const task = await Task.findById(_id)
+    const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
     if (!task) return res.status(404).send()
     updates.forEach(update => task[update] = req.body[update])
     await task.save()
@@ -55,10 +57,9 @@ router.patch('/tasks/:id', async (req, res) => {
   }
 })
 
-router.delete('/tasks/:id', async (req, res) => {
-  const _id = req.params.id
+router.delete('/tasks/:id', auth, async (req, res) => {
   try {
-    const task = await Task.findByIdAndDelete(_id)
+    const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id } )
     if (!task) return res.status(404).send()
     res.send(task)
   } catch (e) {
