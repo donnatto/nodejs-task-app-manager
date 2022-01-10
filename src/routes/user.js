2b4beeff812d53352cb417afc257ecfbf271a54b
Edwin Dominguez <edwindominguez@hotmail.com>
2022-01-09 22:16:25 -0500

    add auth for user creation and login

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
@@ -7,8 +8,7 @@ router.post('/users', async (req, res) => {
   try {
     await user.save()
     const token = await user.generateAuthToken()
-    res.send({ user, token })
-    res.status(201).send(user)
+    res.status(201).send({ user, token })
   } catch (e) {
     res.status(400).send(e)
   }
@@ -24,13 +24,8 @@ router.post('/users/login', async (req, res) => {
   }
 })
 
-router.get('/users', async (req, res) => {
-  try {
-    const users = await User.find()
-    res.send(users)
-  } catch (e) {
-    res.status(500).send(e)
-  }
+router.get('/users/me', auth, async (req, res) => {
+  res.send(req.user)
 })
 
 router.get('/users/:id', async (req, res) => {
