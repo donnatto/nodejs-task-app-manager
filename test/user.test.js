c5f64cfdf7329c66265525996eb804dded376d95
Edwin Dominguez <edwindominguez@hotmail.com>
2022-01-12 22:51:02 -0500

    finish user tests

--- test/user.test.js
+++ test/user.test.js
@@ -14,6 +14,7 @@ const userOne = {
     token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
   }]
 }
+const authToken = `Bearer ${userOne.tokens[0].token}`
 
 beforeEach(async () => {
   await User.deleteMany()
@@ -62,7 +63,7 @@ test('Should not login nonexistent user', async () => {
 test('Should get profile for user', async () => {
   await request(app)
     .get('/users/me')
-    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .set('Authorization', authToken)
     .send()
     .expect(200)
 })
@@ -77,7 +78,7 @@ test('Should not get profile for unauthenticated user', async () => {
 test('Should delete account for user', async () => {
   await request(app)
     .delete('/users/me')
-    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .set('Authorization', authToken)
     .send()
     .expect(200)
 
@@ -90,4 +91,38 @@ test('Should not delete account for unauthenticated user', async () => {
     .delete('/users/me')
     .send()
     .expect(401)
+})
+
+test('Should upload avatar image', async () => {
+  await request(app)
+    .post('/users/me/avatar')
+    .set('Authorization', authToken)
+    .attach('avatar', 'test/fixtures/profile-pic.jpg')
+    .expect(200)
+
+  const user = await User.findById(userOneId)
+  expect(user.avatar).toEqual(expect.any(Buffer))
+})
+
+test('Should update valid user fields', async () => {
+  await request(app)
+    .patch('/users/me')
+    .set(`Authorization`, authToken)
+    .send({
+      name: 'Mike'
+    })
+    .expect(200)
+
+  const user = await User.findById(userOneId)
+  expect(user.name).toBe('Mike')
+})
+
+test('Should not update invalid user fields', async () => {
+  await request(app)
+    .patch('/users/me')
+    .set('Authorization', authToken)
+    .send({
+      location: 'Lima, Peru'
+    })
+    .expect(400)
 })
